acc2f8187096feaa4006b709aaaac6d72fd84885
Bruno Peña <brunoph26@gmail.com>
2024-10-16 15:36:29 -0500

    feat: add technologies tag legend

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,25 @@ module.exports = {
       animation: {
         fade: 'fadeOut 5s ease-in-out forwards'
       },
-
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+      },
+      safelist: [
+        {
+          pattern: /bg-(red|blue|yellow|purple|orange|gray|green|pink)-(500|600|700|800)/,
+        }
+      ],
       keyframes:  {
         fadeOut: {
           from: { opacity: 1 },
